76dcdffcf64f6a3272b2ee48bb0dfee2e2fa84a4
Saurabh Gupta <100rabhgupta8400@gmail.com>
2026-06-17 13:47:30 +0530

    Time: 29 ms (5.70%) | Memory: 47 MB (7.60%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string processStr(string s) {
-        string ans="";
-        for ( int i =0;i<s.size();i++){
-             if ( s[i]=='*'){
-                 if (ans.empty()) continue;
-                string temp= ans;
-                ans="";
-                for ( int i=0;i<temp.size()-1;i++){
-                    ans+=temp[i];
-                }
+        string ans = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '*') {
+                if (ans.empty())
+                    continue;
 
-             }
-             else if ( s[i]=='#'){
-               if (ans.empty()) continue;
-                string temp= ans;
-                ans+=temp;
-             }
-             else if( s[i]=='%'){
-               if (ans.empty()) continue;
-               reverse(ans.begin(),ans.end());
-             }
-             else {
-                 ans+=s[i];
-             }
-             cout<<ans<<endl;
+                ans.pop_back();
+
+            } else if (s[i] == '#') {
+                if (ans.empty())
+                    continue;
+                string temp = ans;
+                ans += temp;
+
+            } else if (s[i] == '%') {
+                if (ans.empty())
+                    continue;
+                reverse(ans.begin(), ans.end());
+            } else {
+                ans.push_back(s[i]);
+            }
+            cout << ans << endl;
         }
         return ans;
-        
     }
 };
