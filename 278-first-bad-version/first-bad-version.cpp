4a362cb22175a19514e734d4b8fc602bff04180f
Saurabh Gupta <100rabhgupta8400@gmail.com>
2026-09-05 00:00:08 +0530

    Time: 2 ms (57.58%) | Memory: 7.7 MB (99.84%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,27 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long long low = 1;
+        long long high = n;
+
+        while (low < high) {
+
+            long long mid = low + (high - low) / 2;
+
+            if (isBadVersion(mid)) {
+                // mid is bad, so answer is mid or before it
+                high = mid;
+            }
+            else {
+                // mid is good, so answer must be after mid
+                low = mid + 1;
+            }
+        }
+
+        return low;
+        
+    }
+};
